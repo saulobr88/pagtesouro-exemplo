6ae1428beeadba1c53262753b9466d4b17b1baa5
Saulo Gomes <saulo.gomes@reitoria.ifpe.edu.br>
2023-08-28 12:00:39 -0300

    Adicionado template/layouts/base

--- Cliente/client/urls.py
+++ Cliente/client/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from .views import PaginaInicial
 
 urlpatterns = [
-    
+    path('', PaginaInicial.as_view(), name="index")
 ]
--- Cliente/client/views.py
+++ Cliente/client/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+class PaginaInicial(TemplateView):
+    # Toda classe filha do TemplateView precisa do
+    # atributo abaixo para definir um template a ser renderizado
+    template_name = 'client/index.html'
